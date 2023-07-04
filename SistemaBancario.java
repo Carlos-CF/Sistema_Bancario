6ac5846e0d43382b5dc79d3e48632e18133e6304
Carlos Fernandes <carloscaetanofernandes@hotmail.com>
2023-07-04 14:02:46 -0300

    funções do menu

--- SistemaBancario.java
+++ SistemaBancario.java
@@ -19,13 +19,36 @@ public class SistemaBancario {
         String menu = """
                    ** Digite sua opção **
                    1 - Consultar saldo
-                   2 - Transferir valor
-                   3 - Receber valor
+                   2 - Receber valor
+                   3 - Transferir valor
                    4 - Sair
                 """;
 
         while (opcao != 4) {
             System.out.println(menu);
-            opcao = scanner.nextInt();}
+            opcao = scanner.nextInt();
+
+            if (opcao == 1) {
+                System.out.println("O saldo atual da sua conta é: " + saldo);
+            } else if (opcao == 2) {
+                System.out.println("Qual é o valor que deseja receber:");
+                double valor = scanner.nextDouble();
+                saldo = saldo + valor;
+                System.out.println("Novo saldo: " + saldo);
+            } else if (opcao == 3) {
+                System.out.println("Qual o Valor que deseja transferir:");
+                double valor = scanner.nextDouble();
+
+                if (valor < saldo) {
+                    saldo = saldo - valor;
+                    System.out.println("Transferencia com sucesso.");
+                    System.out.println("Saldo Atual: " + saldo);
+                } else {
+                    System.out.println("Valor da Transferencia maior que o saldo atual");
+                }
+            } else if (opcao != 4) {
+                System.out.println("Opção inválida!");
+            }
+        }
     }
 }
