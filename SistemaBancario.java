18d262f9efa2b98a0c7a8e7a673c0866d563a378
Carlos Fernandes <carloscaetanofernandes@hotmail.com>
2023-07-04 13:45:09 -0300

    Menu de opções

--- SistemaBancario.java
+++ SistemaBancario.java
@@ -7,6 +7,7 @@ public class SistemaBancario {
         String nome =  "Carlos Fernandes";
         String tipoconta = "Conta Corrente";
         double saldo = 2000;
+        int opcao = 0;
 
         System.out.println("**************************");
         System.out.println("\nDados inicias do cliente:");
@@ -14,5 +15,17 @@ public class SistemaBancario {
         System.out.println("Tipo de Conta: " + tipoconta);
         System.out.println("Saldo Atual: " + saldo);
         System.out.println("\n**************************");
+
+        String menu = """
+                   ** Digite sua opção **
+                   1 - Consultar saldo
+                   2 - Transferir valor
+                   3 - Receber valor
+                   4 - Sair
+                """;
+
+        while (opcao != 4) {
+            System.out.println(menu);
+            opcao = scanner.nextInt();}
     }
 }
